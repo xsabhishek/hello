6c3085a8b96973e24761b18580c1d30c08e06db4
xsabhishek <38415047+xsabhishek@users.noreply.github.com>
2018-04-16 00:19:42 -0400

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,14 @@
+// var assert = require('chai').assert;
+// var first = require('../first');
+//
+// // describe('First',function(){
+// //    it('first should return hello world'),function(){
+// //      assert.equal(first(),'hello world');
+// //    }
+// // })
+// describe('myCalc',function(){
+//    it('first should return the sum',function(){
+//      var result = myCalc('+',1,1);
+//      assert.equal(result,2,'the result is 2');
+//    });
+// });
